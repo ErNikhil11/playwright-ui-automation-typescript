1754e54e8446b97464631c4e0997af2e75501d76
Nikhil Kumar <106591481+ErNikhil11@users.noreply.github.com>
2025-07-19 23:29:27 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,55 @@
-import { defineConfig } from '@playwright/test';
-import dotenv from 'dotenv';
+import { defineConfig, devices } from "@playwright/test";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL: process.env.BASE_URL,
-    extraHTTPHeaders: {
-      'Content-Type': 'application/json'
-    }
+    browserName: "chromium",
+    headless: true,
+    screenshot: "only-on-failure",
   },
-  reporter: [['list'], ['allure-playwright']],
+  reporter: [["list"], ["allure-playwright"]],
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
 });
